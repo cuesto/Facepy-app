f0df704313a36f612d753a8f8def18be9288ce17
Jonathan Cuesto <34cuesto@gmail.com>
2017-07-01 13:19:54 -0400

    AndHUD added

--- Facepy/Facepy.Android/MainActivity.cs
+++ Facepy/Facepy.Android/MainActivity.cs
@@ -6,32 +6,39 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using AndroidHUD;
+using Plugin.Media;
 
 namespace Facepy.Droid
 {
-	[Activity (Label = "Facepy", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    [Activity(Label = "Facepy", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
         Button btnTakePic;
 
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			btnTakePic = FindViewById<Button> (Resource.Id.btnTakePic);
+            // Get our button from the layout resource,
+            // and attach an event to it
+            btnTakePic = FindViewById<Button>(Resource.Id.btnTakePic);
 
             btnTakePic.Click += BtnTakePic_Click;
-		}
+            AndHUD.Shared.Show(this, "Initializing");
+            AndHUD.Shared.Dismiss();
+        }
 
-        private void BtnTakePic_Click(object sender, EventArgs e)
+        async void BtnTakePic_Click(object sender, EventArgs e)
         {
-            btnTakePic.Text = string.Format("{0} clicks!", count++);
+            if (CrossMedia.Current.IsPickPhotoSupported)
+            {
+                btnTakePic.Text = string.Format("{0} clicks!", count++);
+            }
         }
     }
 }
